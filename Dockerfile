1c1f4e9ed8ff9efb1e06db67938d1a0845b5c755
Paul Kluitenberg <paul.kluitenberg@gmail.com>
2022-12-15 19:04:10 +0100

    I may actually just need to copy the nginx config

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
 FROM nginx:alpine
+# copy nginx configuration
+COPY ./nginx.conf /usr/share/nginx/nginx.conf
+#copying assets
 COPY images /usr/share/nginx/html/images
 COPY public /usr/share/nginx/html/public
 COPY scripts /usr/share/nginx/html/scripts
 COPY styles /usr/share/nginx/html/styles
-COPY html /usr/share/nginx/html/html
-COPY index.html /usr/share/nginx/html
+COPY *.html /usr/share/nginx/html/html
--- scripts/openOverlay.js
+++ scripts/openOverlay.js
@@ -1,12 +1,15 @@
 function openOverlay(imgSrc, imgLocation, imgTimestamp) {
   document.getElementById("overlay").style.display = "block";
-  document.getElementById("fullImage").src=imgSrc;
-  document.getElementById("location").innerHTML=imgLocation;
-  document.getElementById("timestamp").innerHTML=imgTimestamp;
+  document.getElementById("fullImage").src = imgSrc;
+  document.getElementById("location").innerHTML = imgLocation;
+  document.getElementById("timestamp").innerHTML = imgTimestamp;
   document.getElementById("body").style.overflow = "hidden";
 }
 
 function closeOverlay() {
   document.getElementById("overlay").style.display = "none";
+  document.getElementById("fullImage").src = "";
+  document.getElementById("location").innerHTML = "";
+  document.getElementById("timestamp").innerHTML = "";
   document.getElementById("body").style.overflow = "auto";
 }
