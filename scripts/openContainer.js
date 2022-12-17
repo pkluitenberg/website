72f7b8fbb3943c0ff27857ed62241143d489c4f7
Paul Kluitenberg <paul.kluitenberg@gmail.com>
2022-12-17 18:06:53 +0100

    added aboutme, career, and projects pages

--- scripts/openContainer.js
+++
@@ -1,8 +0,0 @@
-function openContainer(id) {
-    var x = document.getElementById(id);
-    if (x.className.indexOf('hide') != -1) {
-        x.className = x.className.replace(' hide', ' show')
-    } else {
-        x.className = x.className.replace(' show', ' hide');
-    }
-}
